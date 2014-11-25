e16a8f468cec83a9cf868e258d5acce7b483cea9
mytoh <mytoh@gmail.com>
2014-11-25 13:31:46 +0900

    use eshellar utility

--- eshellar/bottles/freebsd/init.el
+++ eshellar/bottles/freebsd/init.el
@@ -2,20 +2,19 @@
 
 ;;; Code:
 
-(cl-defun muki:eshell-add-alias (name def)
-  (add-to-list 'eshell-command-aliases-list (list name def)))
-
 (when (string-equal system-type "berkeley-unix")
-  (muki:eshell-add-alias "pcheck" "sudo portmaster -PBiydav && sudo portaudit -Fdav && sudo portmaster --clean-packages --clean-distfiles" )
-  (muki:eshell-add-alias "pfetch" "sudo make fetch-recursive" )
-  (muki:eshell-add-alias "pinst" "sudo make -s clean reinstall clean distclean BATCH=yes")
-  (muki:eshell-add-alias "pconf" "sudo make config")
-  (muki:eshell-add-alias "pconfr" "sudo make config-recursive")
-  (muki:eshell-add-alias "pclean" "sudo make clean "         )
-  (muki:eshell-add-alias "ppconf" "make pretty-print-config" )
-  (muki:eshell-add-alias "psconf" "make showconfig"          )
-  (muki:eshell-add-alias "prmconf" "make rmconfig"           )
-  (muki:eshell-add-alias "puniname" "make -VUNIQUENAME"))
+  (eshellar:add-alias "pcheck" "sudo portmaster -PBiydav && sudo portaudit -Fdav && sudo portmaster --clean-packages --clean-distfiles" )
+  (eshellar:add-alias "pfetch" "sudo make fetch-recursive" )
+  (eshellar:add-alias "pinst" "sudo make -s clean reinstall clean distclean BATCH=yes")
+  (eshellar:add-alias "pconf" "sudo make config")
+  (eshellar:add-alias "pconfr" "sudo make config-recursive")
+  (eshellar:add-alias "pclean" "sudo make clean "         )
+  (eshellar:add-alias "ppconf" "make pretty-print-config" )
+  (eshellar:add-alias "psconf" "make showconfig"          )
+  (eshellar:add-alias "prmconf" "make rmconfig"           )
+  (eshellar:add-alias "puniname" "make -VUNIQUENAME")
+  (eshellar:add-alias "pm" "sudo portmaster -dBvy"))
 
+(eshellar:add-exec-path "~/.shellar_custom/bottles/freebsd/bin")
 
 ;;; freebsd.el ends here

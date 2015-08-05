f8edb95a81cb40de899d806a292e68946ac1dd18
mytoh <mytoh@gmail.com>
2015-08-05 20:27:16 +0900

    add some bottles

---
+++ eshellar/bottles/clip/init.el
@@ -0,0 +1,11 @@
+;;; init -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Code:
+
+(eshellar:add-alias "clip"
+                    "xclip -o")
+
+(eshellar:add-alias "clip-base"
+                    "xclip -o | xargs basename")
+
+;;; init.el ends here
---
+++ eshellar/bottles/mesh/init.el
@@ -0,0 +1,8 @@
+;;; init -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Code:
+(evil-define-key 'normal eshell-mode-map
+  "H"  #'mesh:prev-tab
+  "L"  #'mesh:next-tab)
+
+;;; init.el ends here

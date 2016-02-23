bf5949c0fca63eab6ac8c158bd59b2c4e238e1e1
mytoh <mytoh@gmail.com>
2016-02-23 10:49:49 +0900

    add eshell-git-prompt

---
+++ eshellar/bottles/eshell-git-prompt/init.el
@@ -0,0 +1,10 @@
+;;; init -- init -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(req 'eshell-git-prompt
+  (eshell-git-prompt-use-theme 'git-radar))
+
+;;; init.el ends here

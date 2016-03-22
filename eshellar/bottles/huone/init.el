49debcfc0dce88319f18aa2ef425e9a0896c661f
mytoh <mytoh@gmail.com>
2016-03-23 03:56:56 +0900

    * eshellar/bottles/huone/init.el: fix

--- eshellar/bottles/huone/init.el
+++ eshellar/bottles/huone/init.el
@@ -2,6 +2,32 @@
 
 ;;; Code:
 
-(setenv "HUONE" (expand-file-name "~/huone"))
+;; (setenv "HUONE" (expand-file-name "~/huone"))
+
+(cl-defun set-exec-path-from-shell-PATH (shell env)
+  (cl-letf* ((option (cond ((string-match-p "tcsh\\'" shell)
+                            "-c")
+                           ((string-match-p "mksh\\'" shell)
+                            "-l -c")
+                           ((string-match-p "fish\\'" shell)
+                            (concat "--interactive --login --command=\"printf '%s' "
+                                    " {$" env "} " "\""))
+                           ;; fish -i -l -c "printf \"__RESULT %s %s\" \"$PATH\" \"$MANPATH\""
+                           (else
+                            "--login -c")))
+             (env-value-from-shell (replace-regexp-in-string
+                                    "[ \t\n]*$" ""
+                                    (shell-command-to-string (concat shell " " option " 'echo $"
+                                                                     env "'")))))
+    (setenv env env-value-from-shell)
+    (if (string-equal env "PATH")
+        (setq exec-path (split-string env-value-from-shell path-separator)))))
+
+(colle:each
+ (lambda (env)
+   (set-exec-path-from-shell-PATH (getenv "SHELL")
+                                  env))
+ '("PATH" "HUONE" "HUONE_OHJELMAT"))
+
 
 ;;; init.el ends here

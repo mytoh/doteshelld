8b5301935d3808b008598a999a26be33719057a3
mytoh <mytoh@gmail.com>
2016-04-01 04:56:30 +0900

    * eshellar/bottles/alias/init.el: add alias

--- eshellar/bottles/alias/init.el
+++ eshellar/bottles/alias/init.el
@@ -3,6 +3,7 @@
 ;;; Code:
 
 (eshellar:add-alias  "img" "insert-image ${create-image ${expand-file-name $1}} ")
+(eshellar:add-alias "imgs" "insert-sliced-image ${create-image ${expand-file-name $1}}  ' ' () 60 1")
 (eshellar:add-alias  "k" "killall -9 $*")
 (eshellar:add-alias  "d" "dired-other-window ${pwd}")
 (eshellar:add-alias  "ff" "find-file $1")
@@ -14,7 +15,7 @@
 (eshellar:add-alias  "af" "ag --nopager -g $*")
 (eshellar:add-alias  "pam" "rm -rf $*")
 (eshellar:add-alias  "ke" "save-buffers-kill-emacs")
-(eshellar:add-alias  "cde" "cd ~/huone/git/git.savannah.gnu.org/emacs")
+(eshellar:add-alias  "cde" "cd ~/.emacs.d/vendor/github.com/emacs-mirror/emacs")
 (eshellar:add-alias  "gpl" "git pull --verbose")
 (eshellar:add-alias  "gst" "git status")
 (eshellar:add-alias  "kuva" "pikkukivi kuva $*")

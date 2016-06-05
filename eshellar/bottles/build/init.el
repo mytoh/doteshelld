e2cf4ef209d68f819f2617dffd1edcfea125a5e8
mytoh <mytoh@gmail.com>
2016-06-05 22:05:10 +0900

    * eshellar/bottles/build/init.el: add vdesk

--- eshellar/bottles/build/init.el
+++ eshellar/bottles/build/init.el
@@ -709,6 +709,19 @@
    "gmake install;" 
    "gmake clean"))
 
+(muki:eshell-define-build-alias
+ :alias "build-vdesk"
+ :repo (muki:build-path-hoarder "offog.org/git/vdesk.git")
+ :commands
+ `("git pull;"
+   "./autogen.sh;"
+   ,(concat "./configure --prefix="
+            (expand-file-name "vdesk" (getenv "HUONE_OHJELMAT"))
+            ";")
+   "gmake;"
+   "gmake install;" 
+   "gmake clean"))
+
 ;; cd ~/huone/git/github.com/knopwob/dunst/ ; gmake clean ; gmake PREFIX=/home/mytoh/huone/ohjelmat/dunst install
 
 ;;; build.el ends here
